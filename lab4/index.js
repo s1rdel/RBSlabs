e62d54d34c01429689611bde5b2bb3552d53c24a
s1rdel <145557857+s1rdel@users.noreply.github.com>
2026-05-09 10:23:41 +0100

    Add files via upload

---
+++ lab4/index.js
@@ -0,0 +1,21 @@
+var app = require('express')();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+
+app.get('/', function(req, res) {
+  res.sendFile(__dirname + '/index.html');
+});
+
+app.get('/about', function(req, res) {
+  res.send('test');
+});
+
+io.on('connection', function(socket) {
+  socket.on('send message', function(msg) {
+    io.emit('receive message', msg);
+  });
+});
+
+http.listen(3000, function() {
+  console.log('listening on *:3000');
+});
